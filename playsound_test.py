4d73139fc995110f47882642344091edd7e0a8cd
Vikram Vasudevan <vikram.vasudevan@ekahaa.com>
2023-09-16 09:33:40 +0530

    fine tuning sound

--- playsound_test.py
+++ playsound_test.py
@@ -1,3 +1,5 @@
+import os
+
 # Import the gTTS module for text  
 # to speech conversion  
 from gtts import gTTS  
@@ -8,7 +10,7 @@ from gtts import gTTS
 from playsound import playsound  
   
 # It is a text value that we want to convert to audio  
-text_val = 'Happy Birthday Sashank Vikram! Best wishes from dad!'  
+text_val = 'First Subcube 1,2,3. Second subcube 4,5,6. Third subcube 7,8,9.'  
   
 # Here are converting in English Language  
 language = 'en'  
@@ -21,7 +23,12 @@ obj = gTTS(text=text_val, lang=language, slow=False)
   
 #Here we are saving the transformed audio in a mp3 file named  
 # exam.mp3  
-obj.save("./output/exam.mp3")  
-  
+soundFileName = "./exam.mp3"
+archiveFileName = "./output/exam.mp3"
+obj.save(soundFileName)
+print("Saved to file")
 # Play the exam.mp3 file  
-playsound("./output/exam.mp3")  
+print("Now Playing ....")
+playsound(soundFileName) 
+
+os.replace(soundFileName,archiveFileName )
